a2181f3e2b53022844b67fa9eecc5c04721e9a7d
Kaeden Lee <02kaeden.lee@gulllakecs.org>
2019-12-03 13:50:47 -0500

    Merged Digital Read & Serial w/ Master

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -4,14 +4,31 @@
  * 
  * This program will turn on an LED for specified amounts of time, turn it
  * off, then back on in a While Loop
+ * 
+ * Merged on 12.2.19
+ * 
+ * Programmer: Kaeden Lee
+ * Date: 11.14.19
+ * Program: DigitalRead() & the Serial Port
+ * 
+ * Reads a digital input on pin2, prints the result
+ *
  */
 
 // Initialize our LED to Pin 13
 int led = 13;
 
+//digital pin2 has a push button attached to it
+int pushButton = 2;
+
 void setup() {
   // Using the Function pinMode to call the variable led and provide Output
   pinMode(led, OUTPUT);
+
+  //initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+ //make the pushButton pin an input
+ pinMode(pushButton, INPUT);
 }
 
 void loop() {
@@ -20,4 +37,10 @@ void loop() {
   delay(1500);
   digitalWrite(led, LOW);
   delay(500);
+
+  // read the input pin
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button
+  Serial.println(buttonState);
+  delay(250); // delay in between readings for stability
 }
