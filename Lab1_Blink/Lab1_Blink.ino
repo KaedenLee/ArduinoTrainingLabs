a350447e6d1a4126b4dfc210cbfe1ca10a9be860
Kaeden Lee <02kaeden.lee@gulllakecs.org>
2019-12-06 15:55:08 -0500

    Potentiometer Working from 0 - 1023

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,17 +1,26 @@
 /* Programmer: Kaeden Lee
- * Date: 12.3.19
- * Program: Potentiometer
- * 
- * AnalogReadSerial
- * Reads an analog input on pin A0, prints to the serial monitor
- */
+   Date: 12.3.19
+   Program: Potentiometer
+
+   AnalogReadSerial
+   Reads an analog input on pin A0, prints to the serial monitor
+
+*/
 
 
 
 void setup() {
-  
+  // Initialize the serial communication at 9600 bits per second
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  // Read the input on analog pin 0
+  int potentiometerValue = analogRead(A0);
+
+  // Print the value of our Potentiometer
+  Serial.println(potentiometerValue);
+
+  // Add a delay for stability
+  delay(1);
 }
