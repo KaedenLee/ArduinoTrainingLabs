8cd0fcbe3e031648c85873b940125a0f70848f01
Kaeden Lee <02kaeden.lee@gulllakecs.org>
2020-01-10 14:57:33 -0500

    Test Project
    Mr. Lange

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,43 +1,7 @@
-/* Programmer: Kaeden Lee
-   Date: 12.10.19
-   Program: LED & Potentiometer
-
-   Read analog port and turn on LED at a determined threshold
-   
-*/
-
-
-const int analogPin = A0;
-const int ledPin = 13;
-float threshold = 2.25;
-
 
 void setup() {
-  //initialize the LED pin as an output
-  pinMode(ledPin, OUTPUT);
-  
-  // Initialize the serial communication at 9600 bits per second
-  Serial.begin(9600);
+
 }
-
 void loop() {
-  // Read the input on analog pin 0
-  int potentiometerValue = analogRead(analogPin);
 
-  //Convert the analog read from 0 - 1023 to 0 - 5 Volts
-  float voltage = potentiometerValue * (5.0 / 1023.0);
-
-  // Print the value of our Potentiometer
-  Serial.println(voltage);
-
-  //if the analog value is higher than Threshold Variable it will turn on the LED
-  if (voltage > threshold) {
-    digitalWrite(ledPin, HIGH);
-  }
-  else {
-    digitalWrite(ledPin, LOW);
-  }
-
-  // Add a delay for stability
-  delay(1);
 }
